c4b65d7aa2c11acfd493a426546e31acbac2e986
Gedeon-yala-kuhanda <gedeonyalakuhanda@gmail.com>
2020-11-16 17:04:55 +0200

    Ajout-commentaire-dans-app

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const app = express();
 const productsRoutes = require('./api/routes/product');
 const ordersRoutes = require('./api/routes/orders');
 
-
 // MIDDLEWARE
 app.use(morgan('dev'));
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
+
+// GESTION DES ERREURS
 app.use((req, res, next) => {
     const error = new Error('THE REQUEST WAS NOT FOUND ON THE SERVER');
     error.status = 404;
