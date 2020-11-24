be9548022771d0e6bd2273ef4abeb8da1014b840
Optimus-ged <gedeonyalakuhanda@gmail.com>
2020-11-24 10:49:52 +0200

    repetition-on-making-order-model

--- api/models/order.js
+++ api/models/order.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
 
-// Comment
-// Order model
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    product: { type: mongoose.Schema.type.ObjectId, ref: 'Product', require: true },
+    product: { type: mongoose.Schema.Types.ObjectId },
     quantity: { type: Number, default: 1 }
 });
 
-// Comment
-// Export for order module
 module.exports = mongoose.model('Order', orderSchema);
